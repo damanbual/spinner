f6ae803b469e5d19a76849bacbd27b00f0a06182
Daman Bual <damanbual@gmail.com>
2024-06-10 12:13:06 -0700

    refactored code for spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,40 +1,54 @@
 
-// Copy of Spinner1 into Spinner2
+// // Copy of Spinner1 into Spinner2
 
-// async/spinner/spinner1.js
+// // async/spinner/spinner1.js
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
 
-setTimeout(() => {
-  process.stdout.write('\r\\   '); // Need to escape the backslash since it's a special character.
-}, 700);
+// setTimeout(() => {
+//   process.stdout.write('\r\\   '); // Need to escape the backslash since it's a special character.
+// }, 700);
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 900);
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1100);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1300);
 
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 1500);
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 1500);
 
-setTimeout(() => {
-  process.stdout.write('\r|   \n'); // Add a newline at the end
-}, 1700);
+// setTimeout(() => {
+//   process.stdout.write('\r|   \n'); // Add a newline at the end
+// }, 1700);
+
+// Refactoring above code
+
+const spinnerChars = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+let delay = 100;
+spinnerChars.forEach((char, index) => {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `);
+    if (index === spinnerChars.length - 1) {
+      process.stdout.write('\n'); // Move to a new line at the end
+    }
+  }, delay);
+  delay += 200;
+});
