ebdb84809b77376980e8eda0e9db173b48a88e48
Daman Bual <damanbual@gmail.com>
2024-06-10 12:03:20 -0700

    added code for exercise

---
+++ spinner1.js
@@ -0,0 +1,38 @@
+
+// async/spinner/spinner1.js
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); // Need to escape the backslash since it's a special character.
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   \n'); // Add a newline at the end
+}, 1700);
